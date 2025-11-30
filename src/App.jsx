8c00bb4012b51907b58eb26efce273a02912f5ec
Ines-Sekfali <ines.sekfali@ensia.edu.dz>
2025-11-30 15:23:11 +0100

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ import SharePoll from './pages/sharePoll/sharePoll';
 import PollLandingPage from './pages/PollLandingPage';
 import DashboardPage from './pages/DashboardPage';
 import PollPreviewPage from './pages/PollPreviewPage';
-import AccessDenied from "./pages/accessDenied/accessDenied";
-import VoteRecorded from "./pages/confirmedPoll/confirmedPoll";
 
 function App() {
   return (
@@ -21,11 +19,6 @@ function App() {
         <Route path="/SharePoll" element={<SharePoll />} />
         <Route path='/DashboardPage' element={<DashboardPage />} />
         <Route path='/pollpreview' element={<PollPreviewPage/>} />
-        <Route path='/pollpreview' element={<PollPreviewPage/>} />
-        <Route path="/access-denied" element={<AccessDenied />} />
-          <Route path="/poll-confirmed" element={<VoteRecorded />} />
-
-
       </Routes>
     </Router>
   )
